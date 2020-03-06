f66c0dfeaa331ad28e0bc8f6a1be7346c40cd223
Alan Ashworth <awashworth927@gmail.com>
2020-03-06 18:44:25 -0500

    - Managed to get string password generated for lower case and upper case characters. - More work needed to generate numbers and symbols.

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// function generatePassword() {
+//   // 1. Initialize pw variable
+//   // 2. Filer out unconfirmed (false) types
+//   // 3. Loop over length call generator function for each type
+//   // 4. Add final pw to the pa variable and return
+//   let generatedPassword = ""; // Initialize empty string to append characters
+//   let length = setPasswordLength();
+//   let lower = confirm("Should include lowercase?"); // Boolean check; filter out if false
+//   let upper = confirm("Should include uppercase?"); // Boolean check; filter out if false
+//   let numerics = confirm("Should include numerics?"); // Boolean check; filter out if false
+//   let specials = confirm("Should include special caracters?"); // Boolean check; filter out if false
+//   console.log(length, lower, upper, numerics, specials); // check input data types
+
+//   const passwordCriteria = lower + upper + numerics + specials; // count the number if inputs
+//   console.log("Password Criteria: ", passwordCriteria); // check the number of inputs
+
+//   // pass prompts and confirms to an array of objects with keys. Then filter out objects set to false [0].
+//   const criteriaArray = [{ lower }, { upper }, { numerics }, { specials }].filter(
+//     item => Object.values(item)[0]
+//   );
+//   console.log("typesArr: ", criteriaArray); // check the number of objects in typesArr
+
+//   // if no Booleans is set to True, do not proceed
+//   // if (passwordCriteria === 0) {
+//   //   return ""; // returns nothing
+//   // }
+
+//   // Looping in Objects - https://zellwk.com/blog/looping-through-js-objects/ -
+//   // for (let i = 0; i < length; i += passwordCriteria) {
+//   //   criteriaArray.forEach(value => {
+//   //     const generatorName = Object.keys(value)[0];
+//   //     generatedPassword += randomCriteriaGenerator[generatorName]();
+//   //   });
+//   // }
+//   // return generatedPassword;
+
+//   // const finalPassword = generatedPassword.slice(0, length);
+//   // return finalPassword;
+// }
+
+const lowerCaseCharCodes = arrayFromLowToHigh(97, 122);
+const upperCaseCharCodes = arrayFromLowToHigh(65, 90);
+const numericCaseCharCodes = arrayFromLowToHigh(48, 57);
+const specialCaseCharCodes = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123,126));
+
 function generatePassword() {
-  // 1. Initialize pw variable
-  // 2. Filer out unconfirmed (false) types
-  // 3. Loop over length call generator function for each type
-  // 4. Add final pw to the pa variable and return
-  let generatedPassword = "";                         // Initialize empty string to append characters
+  let generatedPassword = [];
   let length = setPasswordLength();
-  let lower = confirm("Should include lowercase?");             // Boolean check; filter out if false
-  let upper = confirm("Should include uppercase?");             // Boolean check; filter out if false
-  let numerics = confirm("Should include numerics?");           // Boolean check; filter out if false
-  let specials = confirm("Should include special caracters?");  // Boolean check; filter out if false
-  console.log(length, lower, upper, numerics, specials);        // check input data types
+  let lower = confirm("Should include lowercase?"); // Boolean check; filter out if false
+  let upper = confirm("Should include uppercase?"); // Boolean check; filter out if false
+  let numeric = confirm("Should include numerics?"); // Boolean check; filter out if false
+  let special = confirm("Should include special caracters?"); // Boolean check; filter out if false
+  const passwordCriteria = lower + upper + numeric + special;
 
-  const typesCount = lower + upper + numerics + specials;       // count the number if inputs
-  console.log("typesCount: ", typesCount);                      // check the number of inputs
+  if(passwordCriteria !== 0){
+    let charcodes = lowerCaseCharCodes;
+    // if(lower === true){charcodes = charcodes.concat(lowerCaseCharCodes);}
+    if(upper === true){charcodes = charcodes.concat(upperCaseCharCodes);}
+    if(numeric === true){charcodes.concat(numericCaseCharCodes);}
+    if(special === true){charcodes = charcodes.concat(specialCaseCharCodes);}
 
-  // pass prompts and confirms to an array of objects with keys. Then filter out objects set to false [0].
-  const typesArr = [{ lower }, { upper }, { numerics }, { specials }].filter(
-    item => Object.values(item)[0]
-  );
-  console.log("typesArr: ", typesArr);                          // check the number of objects in typesArr
-
-  // if no Booleans is set to True, do not proceed
-  if (typesCount === 0) {
-    return ""; // returns nothing
+    for (let i = 0; i < length; i++) {
+      const characterCode = charcodes[Math.floor(Math.random() * length)];
+      generatedPassword.push(String.fromCharCode(characterCode));
+    }
+    return generatedPassword.join('');
   }
-
-  // Looping in Objects - https://zellwk.com/blog/looping-through-js-objects/ -
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      const generatorName = Object.keys(type)[0];
-      console.log("generatorName: ", generatorName);
-
-      generatedPassword += randomGenerator[generatorName]();
-    });
-  }
-
-  const finalPassword = generatedPassword.slice(0, length);
-  return finalPassword;
+  return "";// Returns Nothing
 }
 
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -56,15 +92,13 @@ generateBtn.addEventListener("click", writePassword);
 // Prompts --------------------------------------------------------------------
 
 function setPasswordLength(inputlength) {
-  var inputlength = parseInt(prompt(
-    "Set password length.\nCannot be less than 8 or greater than 128 characters:"
-  ));
+  var inputlength = parseInt(
+    prompt(
+      "Set password length.\nCannot be less than 8 or greater than 128 characters:"
+    )
+  );
 
-  if (
-    inputlength != null &&
-    inputlength > 7 &&
-    inputlength < 129
-  ) {
+  if (inputlength != null && inputlength > 7 && inputlength < 129) {
     return inputlength;
   } else {
     setPasswordLength();
@@ -73,11 +107,11 @@ function setPasswordLength(inputlength) {
 
 // Random Object --------------------------------------------------------------
 // Pass the values returned by the Generators to a randomGenerator object.
-const randomGenerator = {
-  lower: getRandomLower(),
-  upper: getRandomUpper(),
-  number: getRandomNumber(),
-  special: getRandomSpecialCharacter()
+const randomCriteriaGenerator = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecialCharacter
 };
 
 // Generators ----- https://www.w3schools.com/html/html_charset.asp -----------
